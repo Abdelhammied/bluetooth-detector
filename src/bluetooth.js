5b649df93c7c125305f1bb23006ee3aabd29d054
Abdelhammied Elsayed <abdelhammied@gmail.com>
2020-10-05 01:15:13 +0200

    init

--- src/bluetooth.js
+++ src/bluetooth.js
@@ -1,7 +1,6 @@
 const noble = require('@abandonware/noble');
 const io = require('socket.io')(3000);
 
-
 noble.on('stateChange', async(state) => {
     if (state === 'poweredOn') {
         console.log('Bluetooth is up and running');
@@ -11,6 +10,16 @@ noble.on('stateChange', async(state) => {
 });
 
 noble.on('discover', async(peripheral) => {
+    await noble.stopScanningAsync();
+    await peripheral.connectAsync();
+    const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(['180f'], ['2a19']);
+    const batteryLevel = (await characteristics[0].readAsync())[0];
+
     console.log(peripheral.address, 'Detected');
+
     io.emit('discover', peripheral);
+
+    await peripheral.disconnectAsync();
+    process.exit(0);
+
 });
